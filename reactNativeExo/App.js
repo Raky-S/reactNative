3914942bec49d8e44cbc175009f8880c81651633
Raky-S <rakysy1@gmail.com>
2020-10-27 16:57:59 +0100

    commit du 27/10/20

--- reactNativeExo/App.js
+++ reactNativeExo/App.js
@@ -1,60 +1,79 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Button, Alert, ActivityIndicator, require } from 'react-native';
-
-const App = () => {
-  return (
-    <ScrollView style={styles.container} >
-      <View>
-        <Text style={styles.police} >Hello</Text>
-        <Text style={styles.centre} >je suis </Text> {/*on arrive pas a mettre le texte au centre*/}
-        <Text style={styles.gras} >KONEXIO</Text>
-
-        <Image source={require('./assets/konexio-logo_1.png')} />  {/*celle ci ne s'affiche pas*/}
-        <Image
-          source={{ uri: 'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo.png' }}
-          style={{ width: 100, height: 100 }} />
-
-        <Button
-          title="ALERTE"
-          onPress={() => Alert.alert('Simple Button pressed')}
-        // poser question pour console.log   
-        />
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-
-        <ActivityIndicator size="large" color="#00ff00" />
-
-      </View>
-    </ScrollView>
-  );
-};
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Button,
+  Alert,
+  ActivityIndicator,
+  require,
+} from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'blue',
-    className: 'row'
+    backgroundColor: "blue",
+    className: "row",
   },
-
-
 
   police: {
     fontSize: 30,
     paddingBottom: 80,
-    paddingTop: 80
+    paddingTop: 80,
   },
 
   centre: {
-    justifyContent: 'center',
+    display: 'flex',
+    justifyContent: "center",
     alignItems: "center",
     paddingBottom: 80,
   },
 
   gras: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     paddingBottom: 80,
-  }
+  },
+})
+
+class App extends React.Component {
 
 
+  render() {
 
-});
+    return (
+      <ScrollView style={styles.container}>
+        <View>
+          <Text style={styles.police}>Hello</Text>
+          <Text style={styles.centre}>je suis </Text>
+          <Text style={styles.gras}>KONEXIO</Text>
+          <Image source={require("./assets/konexio-logo_1.png")} style={{ width: 300, height: 100 }} />
+          <Image
+            source={{
+              uri:
+                "https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo.png",
+            }}
+            style={{ width: 300, height: 100 }}
+          />
+          <Button
+            title="ALERTE"
+            onPress={() => Alert.alert("Simple Button pressed")}
+          />
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+          <ActivityIndicator size="large" color="#00ff00" />
+        </View>
+      </ScrollView>
+    )
+  };
 
-export default App
+
+}
+export default App;
